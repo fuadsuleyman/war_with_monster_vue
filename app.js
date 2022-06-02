8db5a1f06f24199bb877b946ddb47c4063b919af
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-02 20:17:52 +0400

    Game status my first solution

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ function getRamdomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function checkWinner(playerHealth, monsterHealth) {
+  console.log("Entered to checkwinner");
+  if (playerHealth < 0 && monsterHealth < 0) {
+    return "DRAW";
+  } else if (monsterHealth <= 0 && playerHealth > 0) {
+    return gameStatus = "YOU WIN";
+  } else if (playerHealth <= 0 && monsterHealth > 0) {
+    return gameStatus = "YOU LOSE";
+  } else {
+    return "Game contiunie";
+  }
+}
+
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      gameStatus: "Game contiunie",
     };
   },
   computed: {
@@ -20,6 +34,9 @@ const app = Vue.createApp({
     disableStyle() {
       return this.currentRound % 3 !== 0;
     },
+    getGameStatus() {
+      return this.gameStatus;
+    },
   },
   methods: {
     attackMonster() {
@@ -30,6 +47,9 @@ const app = Vue.createApp({
       console.log(`Player Attacked: / Monster Health: ${this.monsterHealth}`);
       this.attackPlayer();
       console.log(`Monster Attacked: / Player Health: ${this.playerHealth}`);
+      this.gameStatus = checkWinner(this.playerHealth, this.monsterHealth);
+      console.log(`checkWinner: ${checkWinner(this.playerHealth, this.monsterHealth)}`);
+      console.log(`gameStatus: ${this.gameStatus}`);
     },
     attackPlayer() {
       let monsterAttackValue = getRamdomValue(8, 15);
@@ -43,18 +63,24 @@ const app = Vue.createApp({
       console.log(`Special Attacked: / Monster Health: ${this.monsterHealth}`);
       this.attackPlayer();
       console.log(`Monster Attacked: / Player Health: ${this.playerHealth}`);
+      this.gameStatus = checkWinner(this.playerHealth, this.monsterHealth);
+      console.log(`checkWinner: ${checkWinner(this.playerHealth, this.monsterHealth)}`);
+      console.log(`gameStatus: ${this.gameStatus}`);
     },
     healPlayer() {
       this.currentRound++;
-      
+
       let healValue = getRamdomValue(8, 15);
 
-      if (this.playerHealth + healValue > 100){
-          this.playerHealth = 100
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
       } else {
-        this.playerHealth += healValue
+        this.playerHealth += healValue;
       }
       this.attackPlayer;
+      this.gameStatus = checkWinner(this.playerHealth, this.monsterHealth);
+      console.log(`checkWinner: ${checkWinner(this.playerHealth, this.monsterHealth)}`);
+      console.log(`gameStatus: ${this.gameStatus}`);
     },
   },
 });
