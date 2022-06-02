740bbd9f265f423dd52389786c340ad0050331cb
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-02 15:39:54 +0400

    special attack is added

--- app.js
+++ app.js
@@ -7,32 +7,42 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
-    monsterBarStyle(){
-        return {width: this.monsterHealth + '%'}
+    monsterBarStyle() {
+      return { width: this.monsterHealth + "%" };
     },
-    playerBarStyle(){
-        return {width: this.playerHealth + '%'}
+    playerBarStyle() {
+      return { width: this.playerHealth + "%" };
+    },
+    disableStyle() {
+      return this.currentRound % 3 !== 0
     },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
+      console.log(`remainder: ${this.currentRound % 3}`);
       let onceAttackValue = getRamdomValue(5, 12);
-      console.log(this.mandarin);
       this.monsterHealth -= onceAttackValue;
-      console.log(
-        `Player Attacked: / Monster Health: ${this.monsterHealth}`
-      );
+      console.log(`Player Attacked: / Monster Health: ${this.monsterHealth}`);
       this.attackPlayer();
-      console.log(
-        `Monster Attacked: / Player Health: ${this.playerHealth}`
-      );
+      console.log(`Monster Attacked: / Player Health: ${this.playerHealth}`);
     },
     attackPlayer() {
       let monsterAttackValue = getRamdomValue(8, 15);
       this.playerHealth -= monsterAttackValue;
+    },
+    specialAttack() {
+      this.currentRound++;
+      console.log(`remainder: ${this.currentRound % 3}`);
+      let specialAttackValue = getRamdomValue(10, 25);
+      this.monsterHealth -= specialAttackValue;
+      console.log(`Special Attacked: / Monster Health: ${this.monsterHealth}`);
+      this.attackPlayer();
+      console.log(`Monster Attacked: / Player Health: ${this.playerHealth}`);
     },
   },
 });
